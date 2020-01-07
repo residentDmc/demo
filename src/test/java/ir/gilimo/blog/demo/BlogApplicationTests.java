dcace6ff5419103b953823b08576d2a3204386fe
s.sajadi@kintechmail.com <0622sajad>
2020-01-07 16:32:42 +0330

    Initial commit

---
+++ src/test/java/ir/gilimo/blog/demo/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.gilimo.blog.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
